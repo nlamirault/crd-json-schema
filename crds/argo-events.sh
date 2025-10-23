82106d9d622390e822e3d2509917d56dcffdca78
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2025-10-23 14:45:26 +0200

    feat(crd): Add Argo Events
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

---
+++ crds/argo-events.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: Copyright (C) Nicolas Lamirault <nicolas.lamirault@gmail.com>
+# SPDX-License-Identifier: Apache-2.0
+
+export separated=0
+export FILES=(
+  "argoproj.io_eventbus.yaml"
+  "argoproj.io_eventsources.yaml"
+  "argoproj.io_sensors.yaml"
+)
+
+# renovate: datasource=github-tags depName=argoproj/argo-events
+export VERSION=1.9.7
+
+function generate_url {
+  local crd_file=$1
+  echo "https://raw.githubusercontent.com/argoproj/argo-events/refs/tags/v${VERSION}/manifests/base/crds/${crd_file}"
+}

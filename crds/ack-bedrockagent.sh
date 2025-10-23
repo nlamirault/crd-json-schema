3b3409fed80f26525c87f541ed02a66be414fff4
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2025-10-23 18:26:28 +0200

    feat(crd): add ACK Bedrock Agent controller
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

---
+++ crds/ack-bedrockagent.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: Copyright (C) Nicolas Lamirault <nicolas.lamirault@gmail.com>
+# SPDX-License-Identifier: Apache-2.0
+
+export separated=0
+export FILES=(
+  "bedrockagent.services.k8s.aws_agents.yaml"
+  "services.k8s.aws_adoptedresources.yaml"
+  "services.k8s.aws_fieldexports.yaml"
+)
+
+# renovate: datasource=github-tags depName=aws-controllers-k8s/bedrockagent-controller
+export VERSION=0.1.2
+
+function generate_url {
+  local crd_file=$1
+  echo "https://raw.githubusercontent.com/aws-controllers-k8s/bedrockagent-controller/refs/tags/v${VERSION}/helm/crds/${crd_file}"
+}

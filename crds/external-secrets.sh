4e32eaae5e3fa4c50959c5c1bbc58cec4f6f180b
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2025-10-23 14:47:39 +0200

    feat(crd): Add External Secrets
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

---
+++ crds/external-secrets.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: Copyright (C) Nicolas Lamirault <nicolas.lamirault@gmail.com>
+# SPDX-License-Identifier: Apache-2.0
+
+export separated=1
+export FILES=(
+  "acraccesstokens.generators.external-secrets.io.yaml"
+  "clusterexternalsecrets.external-secrets.io.yaml"
+  "clustersecretstores.external-secrets.io.yaml"
+  "ecrauthorizationtokens.generators.external-secrets.io.yaml"
+  "externalsecrets.external-secrets.io.yaml"
+  "fakes.generators.external-secrets.io.yaml"
+  "gcraccesstokens.generators.external-secrets.io.yaml"
+  "githubaccesstokens.generators.external-secrets.io.yaml"
+  "passwords.generators.external-secrets.io.yaml"
+  "pushsecrets.external-secrets.io.yaml"
+  "secretstores.external-secrets.io.yaml"
+  "uuids.generators.external-secrets.io.yaml"
+  "vaultdynamicsecrets.generators.external-secrets.io.yaml"
+  "webhooks.generators.external-secrets.io.yaml"
+)
+
+# renovate: datasource=github-tags depName=external-secrets/external-secrets
+VERSION=0.10.4
+
+function generate_url {
+  echo "https://raw.githubusercontent.com/external-secrets/external-secrets/v${VERSION}/deploy/crds/bundle.yaml"
+}

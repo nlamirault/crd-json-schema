84cbbbba1e912fadd546f1bd35901e665a1a71c2
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2025-10-23 13:12:49 +0200

    feat(crd): Add Argo-CD
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

---
+++ crds/argo-cd.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: Copyright (C) Nicolas Lamirault <nicolas.lamirault@gmail.com>
+# SPDX-License-Identifier: Apache-2.0
+
+export separated=0
+export FILES=(
+  "application-crd.yaml"
+  "applicationset-crd.yaml"
+  "appproject-crd.yaml"
+)
+
+# renovate: datasource=github-tags depName=argoproj/argo-cd
+export VERSION=2.13.0
+
+function generate_url {
+  local crd_file=$1
+  echo "https://raw.githubusercontent.com/argoproj/argo-cd/v${VERSION}/manifests/crds/${crd_file}"
+}

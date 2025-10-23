7ad894383273a08ee12332b8da73a4eaae82d0ec
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2025-10-23 14:48:55 +0200

    feat(crd): Add Gateway API
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

---
+++ crds/gateway-api.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: Copyright (C) Nicolas Lamirault <nicolas.lamirault@gmail.com>
+# SPDX-License-Identifier: Apache-2.0
+
+export separated=0
+export FILES=(
+  "gateway.networking.k8s.io_gatewayclasses.yaml"
+  "gateway.networking.k8s.io_gateways.yaml"
+  "gateway.networking.k8s.io_grpcroutes.yaml"
+  "gateway.networking.k8s.io_httproutes.yaml"
+  "gateway.networking.k8s.io_referencegrants.yaml"
+)
+
+# renovate: datasource=github-tags depName=kubernetes-sigs/gateway-api
+export VERSION=1.2.0
+
+function generate_url {
+  local crd_file=$1
+  echo "https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v${VERSION}/config/crd/standard/${crd_file}"
+}
